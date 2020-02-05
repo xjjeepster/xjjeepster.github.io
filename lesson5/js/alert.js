f32ce32c5fb5d65dbeadbad95f73569b035e61ea
Ryan Hill Account <xjjeepster@byui.edu>
2020-02-05 11:14:34 -0700

    banner alert fixed

--- lesson5/js/alert.js
+++
@@ -1,8 +0,0 @@
-const today = new Date();
-const dayNumber = today.getDay();
-const element = document.getElementById("banner")
-if (dayNumber == 5) {
-    element.classList.add("showme")
-} else {
-    element.classList.add("hideme")
-}
--- lesson5/js/todaysdate.js
+++ lesson5/js/todaysdate.js
@@ -2,3 +2,12 @@ var today = new Date();
 // var formatOptions = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', };
 var formatOptions = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' }
 document.write(today.toLocaleDateString('en-US', formatOptions));
+
+
+const dayNumber = today.getDay();
+const element = document.getElementById("banner")
+if (dayNumber == 5) {
+    element.classList.add("showme")
+} else {
+    element.classList.add("hideme")
+}
