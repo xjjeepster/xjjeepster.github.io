52ecb5f4d3332fb68adc358ea8ff86e5ba195828
Ryan Hill Account <xjjeepster@byui.edu>
2020-02-14 14:03:02 -0700

    updates

--- lesson6/js/scripts.js
+++ lesson6/js/scripts.js
@@ -1,3 +1,15 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+const tempNumber = parseFloat(document.getElementById('temp').textContent);
+const speedNumber = parseFloat(document.getElementById('speed').textContent);
+
+let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 0.16)) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+windchill = Math.round(windchill);
+
+if (tempNumber <= 50 && speedNumber > 3) {
+    document.getElementById("chill").innerHTML = "Wind Chill: " + windchill + " \u00B0";
+} else {
+    document.getElementById("chill").innerHTML = "No Wind Chill Today";
+}
