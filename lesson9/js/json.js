207aa11da64198f21acb4da2202b9357a97255e5
Ryan Hill Account <xjjeepster@byui.edu>
2020-03-05 14:01:54 -0700

    update json

--- lesson9/js/json.js
+++ lesson9/js/json.js
@@ -5,38 +5,40 @@ fetch(requestURL)
         return response.json();
     })
     .then(function(jsonObject) {
-        console.table(jsonObject); // temporary checking for valid response and data parsing
-        const towndata = jsonObject['towns'];
-        //console.log(towndata);
+        //console.table(jsonObject); // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'];
+        //console.log(towns);
 
-        let pagetowns = [towndata[4], towndata[5], towndata[1]];
-        console.log(pagetowns);
+        // let pagetowns = [towndata[4], towndata[5], towndata[1]];
+        //console.log(pagetowns);
 
-        for (let i = 0; i < pagetowns.length; i++) {
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let motto = document.createElement('p');
-            let yf = document.createElement('p');
-            let cp = document.createElement('p');
-            let avgrf = document.createElement('p');
-            let img = document.createElement('img');
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs") {
+                let card = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let motto = document.createElement('p');
+                let yf = document.createElement('p');
+                let cp = document.createElement('p');
+                let avgrf = document.createElement('p');
+                let img = document.createElement('img');
 
-            h2.textContent = pagetowns[i].name;
-            motto.textContent = pagetowns[i].motto;
-            yf.textContent = "Year Founded: " + pagetowns[i].yearFounded;
-            cp.textContent = "Population: " + pagetowns[i].currentPopulation;
-            avgrf.textContent = "Annual Rainfall: " + pagetowns[i].averageRainfall;
+                h2.textContent = towns[i].name;
+                motto.textContent = towns[i].motto;
+                yf.textContent = "Year Founded: " + towns[i].yearFounded;
+                cp.textContent = "Population: " + towns[i].currentPopulation;
+                avgrf.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
 
-            img.setAttribute('src', 'images/' + pagetowns[i].photo);
-            img.setAttribute('alt', pagetowns[i].name);
+                img.setAttribute('src', 'images/' + towns[i].photo);
+                img.setAttribute('alt', towns[i].name);
 
-            card.appendChild(h2);
-            card.appendChild(motto);
-            card.appendChild(yf);
-            card.appendChild(cp);
-            card.appendChild(avgrf);
-            card.appendChild(img);
+                card.appendChild(h2);
+                card.appendChild(motto);
+                card.appendChild(yf);
+                card.appendChild(cp);
+                card.appendChild(avgrf);
+                card.appendChild(img);
 
-            document.querySelector('div.cards').appendChild(card);
+                document.querySelector('div.cards').appendChild(card);
+            }
         }
     });
