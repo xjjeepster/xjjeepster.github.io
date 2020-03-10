62c4864f618188696ec709bd077e80325f2d31a0
Ryan Hill Account <xjjeepster@byui.edu>
2020-03-09 18:20:42 -0600

    Push

--- lesson10/js/weatherSummaryPreston.js
+++ lesson10/js/weatherSummaryPreston.js
@@ -9,8 +9,22 @@ fetch(apiURL2)
         console.log(weatherInfo2);
 
         document.getElementById('currentCond').innerHTML = weatherInfo2.weather[0].main;
-        document.getElementById('temp').innerHTML = weatherInfo2.main.temp;
+        const tempNumber = parseFloat(document.getElementById('temp').textContent = weatherInfo2.main.temp);
         document.getElementById('humidity').innerHTML = weatherInfo2.main.humidity;
-        document.getElementById('speed').innerHTML = weatherInfo2.wind.speed;
+        const speedNumber = parseFloat(document.getElementById('speed').textContent = weatherInfo2.wind.speed);
+
+        //const tempNumber = temp;
+        //const speedNumber = speed;
+        console.log(tempNumber);
+        console.log(speedNumber);
+
+        let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 0.16)) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+        windchill = Math.round(windchill);
+
+        if (tempNumber <= 50 && speedNumber > 3) {
+            document.getElementById("chill").innerHTML = "Wind Chill: " + windchill + " \u00B0";
+        } else {
+            document.getElementById("chill").innerHTML = "No Wind Chill Today";
+        }
 
     }); //end of "then" fat arrow function
