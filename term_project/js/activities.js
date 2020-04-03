bf6743bf18ec428d6f3bc778e1aeaf14f3a65dd8
Ryan Hill Account <xjjeepster@byui.edu>
2020-04-03 13:45:53 -0600

    json

--- term_project/js/activities.js
+++ term_project/js/activities.js
@@ -7,19 +7,19 @@ fetch(requestURL)
     .then(function(jsonObject) {
         // console.table(jsonObject); // temporary checking for valid response and data parsing
         const activities = jsonObject['activities'];
-        for (let i = 0; i < prophets.length; i++) {
+        for (let i = 0; i < activities.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
             let pd = document.createElement('p');
             let pp = document.createElement('p');
             let img = document.createElement('img');
 
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            pd.textContent = "Date of Birth: " + prophets[i].birthdate;
-            pp.textContent = "Place of Birth: " + prophets[i].birthplace;
+            h2.textContent = activities[i].name;
+            pd.textContent = "Description: " + activities[i].description;
+            pp.textContent = "Number fo Visitors (anually): " + activities[i].#visitors;
 
-            img.setAttribute('src', prophets[i].imageurl);
-            img.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + - +prophets[i].order);
+            img.setAttribute('src', activities[i].imageurl);
+
 
 
             card.appendChild(h2);
