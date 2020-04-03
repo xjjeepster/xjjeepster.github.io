109764ecc7bbb8f47a280ddc26316094ad39b93d
Ryan Hill Account <xjjeepster@byui.edu>
2020-04-03 13:27:43 -0600

    json

---
+++ term_project/js/activities.js
@@ -0,0 +1,32 @@
+const requestURL = 'https://xjjeepster.github.io/term_project/data/employees.json';
+
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
+        // console.table(jsonObject); // temporary checking for valid response and data parsing
+        const activities = jsonObject['activities'];
+        for (let i = 0; i < prophets.length; i++) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let pd = document.createElement('p');
+            let pp = document.createElement('p');
+            let img = document.createElement('img');
+
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            pd.textContent = "Date of Birth: " + prophets[i].birthdate;
+            pp.textContent = "Place of Birth: " + prophets[i].birthplace;
+
+            img.setAttribute('src', prophets[i].imageurl);
+            img.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + - +prophets[i].order);
+
+
+            card.appendChild(h2);
+            card.appendChild(pd);
+            card.appendChild(pp);
+            card.appendChild(img);
+
+            document.querySelector('div.cards').appendChild(card);
+        }
+    });
